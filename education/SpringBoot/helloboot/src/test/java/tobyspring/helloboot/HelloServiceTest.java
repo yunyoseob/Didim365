6f3485993d91a83bd8a35b589827a53e56518f67
Yun-Yoseob <imyoseob@gmail.com>
2023-05-08 02:37:03 +0900

    ..

---
+++ education/SpringBoot/helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -0,0 +1,27 @@
+package tobyspring.helloboot;
+
+import tobyspring.helloboot.ComplexHelloService;
+import tobyspring.helloboot.SimpleHelloService;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloServiceTest {
+    @Test
+    void simpleHelloService(){
+        System.out.println("==== simpleHelloService ====");
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        String ret = helloService.sayHello("Test");
+        assertThat(ret).isEqualTo("Simple HelloTest");
+    }
+
+    @Test
+    void complexHelloService(){
+        System.out.println("==== complexHelloService ====");
+        ComplexHelloService helloService = new ComplexHelloService();
+
+        String ret = helloService.sayHello("Test");
+        assertThat(ret).isEqualTo("Complex HelloTest");
+    }
+}
