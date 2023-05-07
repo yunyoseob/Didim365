943a9ea5acf03128fed3dfcaeb071d2b49fe3dd5
Yun-Yoseob <imyoseob@gmail.com>
2023-05-07 23:45:51 +0900

    230507

--- education/SpringBoot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ education/SpringBoot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,14 +1,28 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
+
+// @Component
 @RestController
+// @RequestMapping("/hello")
 public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService){
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name){
-        System.out.println("HelloController >> ");
-        System.out.println("input name : "+name);
-        return "Hello"+name;
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
